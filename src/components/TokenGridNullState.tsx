b5ee68ebadf33da1572b3b6d9b47ed4287505943
ejdiv <ejd4@protonmail.com>
2021-02-14 17:49:07 -0800

    merge image changes

--- src/components/TokenGridNullState.tsx
+++ src/components/TokenGridNullState.tsx
@@ -1,18 +1,19 @@
 import classNames from "classnames";
 import styles from "../styles/Home.module.css";
+import Image from "next/image";
 
 interface TokenGridNullStateProps {}
 
 const TokenGridNullState = (props: TokenGridNullStateProps) => {
   return (
     <div className={styles.main}>
-      {/* TODO: add null state illustration */}
+      <Image src="/Untitled-Artwork-off-white.jpg" width={500} height={400} />
       <div className={styles.cartTitleContainer}>
         <p className={classNames(styles.largeFont)}>
           You don't own any NFTs we support.
         </p>
       </div>
-      <p className={classNames(styles.mediumFont, 'externalLink')}>
+      <p className={classNames(styles.mediumFont, "externalLink")}>
         <a href={"https://opensea.io"}>Looking to buy some?</a>
       </p>
     </div>
