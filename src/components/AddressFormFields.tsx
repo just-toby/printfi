3564fb96ccc48d2bb5b7d81fcd1f10495b4c1957
avoocado-dev <promoaguil@gmail.com>
2021-02-26 13:09:45 -0800

    Revamped shipping

--- src/components/AddressFormFields.tsx
+++ src/components/AddressFormFields.tsx
@@ -1,172 +1,146 @@
-import React, { FC } from "react";
-import { Form, Col } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { Form } from "react-bootstrap";
+import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
+import Select from 'react-select';
+import {getCountries, getStates} from 'country-state-picker'
 
-interface AddressFormFieldsProps {
-  setName: (name: string) => void;
-  setEmail: (email: string) => void;
-  setAddress: (address: string) => void;
-  setAddress2: (address2: string) => void;
-  setCity: (city: string) => void;
-  setState: (state: string) => void;
-  setZip: (zip: string) => void;
-  loading: boolean;
-}
+const AddressFormFields = (props) => {
+  const {googleAddress, setGoogleAddress, address} = props;
+  const [countries, setCountries] = useState([]);
+  const [states, setStates] = useState([]);
 
-const AddressFormFields: FC<AddressFormFieldsProps> = (
-  props: AddressFormFieldsProps
-) => {
+  useEffect(() => {
+    if(Object.keys(address).length !== 0 && address.country !== "")
+    {
+      let newStates = getStates(address.country.toLowerCase()).map(state => { 
+        return {value: state, label: state}
+      })
+      setStates(newStates);
+    }
+  }, [address.country])
+
+  useEffect(() => {
+    let baseCountries = getCountries();
+
+    let newCountries = baseCountries.map(country => 
+    {
+
+      return {value: country.code, label: country.name}
+    })
+
+
+    setCountries(newCountries);
+  }, [])
+
+  const handleGoogleAddressChange = (value, action) => {
+    if(action.action === "clear")
+    {
+      setGoogleAddress({})
+    }
+    else 
+    setGoogleAddress(value);
+  }
+
+  const addressChange = (key) => {
+    if(key !== null && key !== "")
+      address.setAddress(key);
+  }
+
   return (
     <div style={{ display: "flex", flexDirection: "column" }}>
-      <Form.Row
-        style={{
-          display: "flex",
-          flexDirection: "row",
+
+      <div style={{ display: "flex", flexDirection: "row" }}>
+
+          <div className="halfInputBox1">
+            <Form.Control
+              type="Name"
+              style={{height: "37px"}}
+              placeholder="First Name"
+              required={true}
+              onChange={e => address.setFirstName(e.target.value)}
+              disabled={props.loading}
+            />
+          </div>
+
+          <div className="halfInputBox2">
+            <Form.Control
+              style={{height: "37px"}}
+              type="Name"
+              placeholder="Last Name"
+              required={true}
+              onChange={e => address.setLastName(e.target.value)}
+              disabled={props.loading}
+            />
+          </div>
+      </div>
+
+      <GooglePlacesAutocomplete
+        apiKey="AIzaSyB587mOW5-3gNFOSPJeGuJhqf0FUj910uM"
+        selectProps={{
+          onInputChange: (key) => addressChange(key),
+          onChange: (value, action) => handleGoogleAddressChange(value, action),
+          backspaceRemovesValue: true,
+          placeholder: "Address",
+          isClearable: true,
+          noOptionsMessage: () => null,
+          components: { DropdownIndicator:() => null, IndicatorSeparator:() => null },
+          value: address.address === "" ? null : {value: address.address, label: address.address},
+          className:"inputBox"
         }}
-      >
-        <Form.Group
-          as={Col}
-          controlId="formGridName"
-          style={{ marginRight: "2rem" }}
-        >
-          <Form.Label>Name</Form.Label>
-          <Form.Control
-            type="Name"
-            placeholder="Name"
-            required={true}
-            onChange={(event: any) => {
-              props.setName(event.target.value);
-            }}
-            disabled={props.loading}
-          />
-        </Form.Group>
+      /> 
 
-        <Form.Group as={Col} controlId="formGridEmail">
-          <Form.Label>Email</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="Enter email"
-            required={true}
-            onChange={(event: any) => {
-              props.setEmail(event.target.value);
-            }}
-            disabled={props.loading}
-          />
-        </Form.Group>
-      </Form.Row>
-
-      <Form.Group controlId="formGridAddress1">
-        <Form.Label>Address</Form.Label>
+      <div className="inputBox">
         <Form.Control
-          placeholder="1234 Main St"
+          style={{height: "37px"}}
+          type="Name"
+          placeholder="Apartment, suite, etc. (optional)"
+          onChange={e => address.setAddress2(e.target.value)}
+          disabled={props.loading}
+        />
+      </div>
+
+      <div className="inputBox">
+        <Form.Control
+          style={{height: "37px"}}
+          type="Name"
+          placeholder="City"
           required={true}
-          onChange={(event: any) => {
-            props.setAddress(event.target.value);
-          }}
+          onChange={e => address.setCity(e.target.value)}
           disabled={props.loading}
+          value={address.city}
         />
-      </Form.Group>
-
-      <Form.Group controlId="formGridAddress2">
-        <Form.Label>Address 2</Form.Label>
-        <Form.Control
-          placeholder="Apartment, studio, or floor"
-          required={false}
-          onChange={(event: any) => {
-            props.setAddress2(event.target.value);
-          }}
-          disabled={props.loading}
+      </div>
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        <Select
+          options={countries}
+          placeholder={"Country"}
+          noOptionsMessage= {() => null}
+          value={address.country === "" ? null: {value: address.country, label: address.country}}
+          onChange={e => address.setCountry(e.value)}
+          className="thirdInputBox"
         />
-      </Form.Group>
 
-      <Form.Row>
-        <Form.Group as={Col} controlId="formGridCity">
-          <Form.Label>City</Form.Label>
+        <Select
+          placeholder={"State"}
+          options={states}
+          noOptionsMessage= {() => null}
+          value={address.state === "" ? null: {value: address.state, label: address.state}}
+          onChange={e => address.setState(e.value)}
+          className="thirdInputBox"
+        />
+
+        <div className="thirdInputBoxEnd">
           <Form.Control
+            style={{height: "37px"}}
+            type="Name"
+            placeholder="Zip Code"
             required={true}
-            onChange={(event: any) => {
-              props.setCity(event.target.value);
-            }}
+            onChange={e => address.setZip(e.target.value)}
             disabled={props.loading}
+            value={address.zip}
           />
-        </Form.Group>
-
-        <Form.Group as={Col} controlId="formGridState">
-          <Form.Label>State</Form.Label>
-          <Form.Control
-            as="select"
-            defaultValue="Choose..."
-            required={true}
-            onChange={(event: any) => {
-              props.setState(event.target.value);
-            }}
-            disabled={props.loading}
-          >
-            <option>Choose...</option>
-            <option value="AL">Alabama</option>
-            <option value="AK">Alaska</option>
-            <option value="AZ">Arizona</option>
-            <option value="AR">Arkansas</option>
-            <option value="CA">California</option>
-            <option value="CO">Colorado</option>
-            <option value="CT">Connecticut</option>
-            <option value="DE">Delaware</option>
-            <option value="DC">District Of Columbia</option>
-            <option value="FL">Florida</option>
-            <option value="GA">Georgia</option>
-            <option value="HI">Hawaii</option>
-            <option value="ID">Idaho</option>
-            <option value="IL">Illinois</option>
-            <option value="IN">Indiana</option>
-            <option value="IA">Iowa</option>
-            <option value="KS">Kansas</option>
-            <option value="KY">Kentucky</option>
-            <option value="LA">Louisiana</option>
-            <option value="ME">Maine</option>
-            <option value="MD">Maryland</option>
-            <option value="MA">Massachusetts</option>
-            <option value="MI">Michigan</option>
-            <option value="MN">Minnesota</option>
-            <option value="MS">Mississippi</option>
-            <option value="MO">Missouri</option>
-            <option value="MT">Montana</option>
-            <option value="NE">Nebraska</option>
-            <option value="NV">Nevada</option>
-            <option value="NH">New Hampshire</option>
-            <option value="NJ">New Jersey</option>
-            <option value="NM">New Mexico</option>
-            <option value="NY">New York</option>
-            <option value="NC">North Carolina</option>
-            <option value="ND">North Dakota</option>
-            <option value="OH">Ohio</option>
-            <option value="OK">Oklahoma</option>
-            <option value="OR">Oregon</option>
-            <option value="PA">Pennsylvania</option>
-            <option value="RI">Rhode Island</option>
-            <option value="SC">South Carolina</option>
-            <option value="SD">South Dakota</option>
-            <option value="TN">Tennessee</option>
-            <option value="TX">Texas</option>
-            <option value="UT">Utah</option>
-            <option value="VT">Vermont</option>
-            <option value="VA">Virginia</option>
-            <option value="WA">Washington</option>
-            <option value="WV">West Virginia</option>
-            <option value="WI">Wisconsin</option>
-            <option value="WY">Wyoming</option>
-          </Form.Control>
-        </Form.Group>
-
-        <Form.Group as={Col} controlId="formGridZip">
-          <Form.Label>Zip</Form.Label>
-          <Form.Control
-            required={true}
-            onChange={(event: any) => {
-              props.setZip(event.target.value);
-            }}
-            disabled={props.loading}
-          />
-        </Form.Group>
-      </Form.Row>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/checkout.tsx
+++ src/pages/checkout.tsx
@@ -1,9 +1,8 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Header from "../components/Header/Header";
 import { CartContext } from "../context/CartContext";
 import { CheckoutButton } from "../components/CheckoutButton";
 import { Rings, useLoading } from "@agney/react-loading";
-import { ConfirmButton } from "../components/ConfirmButton";
 import useCoinbaseCommerceAPI, {
   CoinbaseCommerceAPI,
 } from "../hooks/useCoinbaseCommerceAPI";
@@ -13,6 +12,10 @@ import { isNullOrEmpty } from "../utils/StringUtils";
 import { useRouter } from "next/router";
 import { ToastContainer, toast } from "react-toastify";
 import useAddressState, { AddressStateConfig } from "../hooks/useAddressState";
+import { Button } from "@material-ui/core";
+import {getCountries} from 'country-state-picker'
+import { getCountryFromAlpha3 } from 'iso-country-utils';
+
 
 interface CheckoutPageProps {}
 
@@ -22,6 +25,7 @@ export default function Checkout(props: CheckoutPageProps) {
   const coinbase: CoinbaseCommerceAPI = useCoinbaseCommerceAPI();
   const [loading, setLoading] = useState(false);
   const router = useRouter();
+  const [googleAddress, setGoogleAddress] = useState(null);
 
   const address: AddressStateConfig = useAddressState();
 
@@ -30,11 +34,40 @@ export default function Checkout(props: CheckoutPageProps) {
     indicator: <Rings width="100" />,
   });
 
+  useEffect(() => {
+    if(googleAddress !== null)
+    {
+      if(Object.keys(googleAddress).length === 0)
+      {
+        address.setAddress("");
+        address.setCity("");
+        address.setCountry("");
+        address.setState("");
+      }
+      else 
+      {
+        console.log("googleAdress: ", googleAddress);
+        if(googleAddress.value.terms[0] && googleAddress.value.terms[1])
+          address.setAddress(googleAddress.value.terms[0].value + " " + googleAddress.value.terms[1].value)
+        if(googleAddress.value.terms[2])
+          address.setCity(googleAddress.value.terms[2].value)
+        if(googleAddress.value.terms[3])
+          address.setState(googleAddress.value.terms[3].value)
+        if(googleAddress.value.terms[4])
+        {
+          let alpha2 = getCountryFromAlpha3(googleAddress.value.terms[4].value).alpha2;
+          address.setCountry(alpha2);
+        }
+      }
+    }
+
+  }, [googleAddress])
+
   const onSubmit = (event: any) => {
     event.preventDefault();
     event.stopPropagation();
     const addressJson = {
-      name: address.name,
+      name: address.firstName + address.lastName,
       email: address.email,
       address: address.address,
       address2: address.address2,
@@ -60,13 +93,15 @@ export default function Checkout(props: CheckoutPageProps) {
 
   const hasValidAddress = () => {
     const vals = [
-      address.name,
+      address.firstName,
+      address.lastName,
       address.email,
       address.address,
       address.city,
       address.state,
       address.zip,
     ];
+    if(!address.email.includes("@") || !address.email.includes(".")) return false;
     return vals.filter((value) => isNullOrEmpty(value)).length === 0;
   };
 
@@ -74,9 +109,27 @@ export default function Checkout(props: CheckoutPageProps) {
     <div className="container">
       <Header subPage="cart" />
       <main className="main">
+
         <div className="cartTitleContainer">
           <span className="largeFont">
-            Checkout - Payment
+            Contact Information
+          </span>
+        </div>
+
+        <div className="subInputBox">
+            <Form.Control
+              style={{height: "37px"}}
+              type="Name"
+              placeholder="Email"
+              required={true}
+              onChange={e => address.setEmail(e.target.value)}
+              disabled={loading}
+            />
+          </div>
+
+        <div className="cartTitleContainer">
+          <span className="largeFont">
+            Shipping Information
           </span>
         </div>
         {cart.length === 0 ? (
@@ -87,24 +140,31 @@ export default function Checkout(props: CheckoutPageProps) {
           <div>
             <Form onSubmit={onSubmit}>
               <AddressFormFields
-                setName={address.setName}
+                setFirstname={address.setFirstName}
                 setEmail={address.setEmail}
                 setAddress={address.setAddress}
                 setAddress2={address.setAddress2}
                 setCity={address.setCity}
                 setState={address.setState}
+
+                setGoogleAddress={setGoogleAddress}
+                googleAddress={googleAddress}
                 setZip={address.setZip}
                 loading={loading}
+                address={address}
               />
               <div className={"cartConfirmButton"}>
                 {isNullOrEmpty(chargeId) ? (
-                  <ConfirmButton
-                    title="Submit"
-                    loading={loading}
+                  <Button
+                    color="primary"
+                    variant="outlined"
                     onClick={null}
                     disabled={!hasValidAddress()}
                     type="submit"
-                  />
+                    className="bigButton"
+                  >
+                    Submit
+                  </Button>
                 ) : (
                   <CheckoutButton
                     chargeId={chargeId}
